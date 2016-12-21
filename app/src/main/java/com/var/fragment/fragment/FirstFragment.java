41c7114181d574e79ab7a88dae4e1c65ef32825e
Lynnardo <gauseed@qq.ocm>
2016-12-22 00:15:15 +0800

    添加文件

---
+++ app/src/main/java/com/var/fragment/fragment/FirstFragment.java
@@ -0,0 +1,32 @@
+package com.var.fragment.fragment;
+
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.var.fragment.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class FirstFragment extends Fragment implements View.OnClickListener{
+
+    public FirstFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_first, container, false);
+    }
+
+    @Override
+    public void onClick(View v) {
+        
+    }
+}
